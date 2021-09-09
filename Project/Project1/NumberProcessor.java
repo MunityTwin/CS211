49aded25728e1ac7cba00f62a24ac7d650a18721
MunityTwin <sonylamphan@gmail.com>
2021-09-08 22:35:57 -0400

    Project 1 (7/10)

--- Project/Project1/NumberProcessor.java
+++ Project/Project1/NumberProcessor.java
@@ -102,8 +102,20 @@ public class NumberProcessor {
 	 * following the collatz game rules starting at the input
 	 */
 	public static int countCollatzSteps(int input){
-		// DELETE THE LINE BELOW ONCE YOU IMPLEMENT THE CALL!
-		throw new RuntimeException("not implemented!");
+		int numSteps = 0;
+		// Repeat the game until the input reach 1
+		while (input != 1) {
+			if (input % 2 == 0) {
+				input /= 2;
+				++numSteps;
+			}
+			else {
+				input = (input * 3) + 1;
+				++numSteps;
+			}
+		}
+
+		return numSteps;
 	}
 
 	/**
@@ -113,24 +125,108 @@ public class NumberProcessor {
 	 * prime numbers as true.
 	 */
 	public static void sieveOfEratosthenes(boolean[] array){
-		// DELETE THE LINE BELOW ONCE YOU IMPLEMENT THE CALL!
-		throw new RuntimeException("not implemented!");
+		// If the array has less than 3 elements, modify all element
+		// to false (the smallest prime number is 2)
+		if (array.length < 3) {
+			for (int i = 0; i < array.length; ++i) {
+				array[i] = false;
+			}
+		}
+		// If the array has at least 3 elements, check if it's prime
+		// indices
+		else {
+			// Smallest prime number is 2
+			array[0] = false;
+			array[1] = false;
+
+			for (int i = 2; i < array.length; ++i) {
+				// Assume the index is a prime number, prove whether the
+				// statement holds.
+				array[i] = true;
+				// Check if the index is a prime numbers
+				for (int j = 2; j < i; ++j) {
+					// If the index is divisible by j (other than itself),
+					// the index is not a prime number
+					if (i % j == 0) {
+						array[i] = false;
+					}
+				}
+			}
+		}
 	}
 
 	/**
 	 * Converts a Nx3 array to the equivalent rectangular array
 	 */
-	 public static int[][] list2array(int[][] list) {
-		// DELETE THE LINE BELOW ONCE YOU IMPLEMENT THE CALL!
-		throw new RuntimeException("not implemented!");
+	public static int[][] list2array(int[][] list) {
+		// Find the total number of rows and columns in the new array
+		int totalRow = 0;
+		int totalCol = 0;
+		int[][] result;
+
+		// The second column of the Nx3 array holds the row position
+		// The third column of the Nx3 array holds the col position
+		// The highest index of rows and columns in the new array will
+		// be the highest number found in the Nx3 array
+		for (int i = 0; i < list.length; ++i) {
+			if (list[i][1] > totalRow) {
+				totalRow = list[i][1];
+			}
+			if (list[i][2] > totalCol) {
+				totalCol = list[i][2];
+			}
+		}
+		// Increment totalRow and totalCol by 1 to find the total
+		++totalRow;
+		++totalCol;
+
+		// Create the new array, initialize all element to 0
+		result = new int[totalRow][totalCol];
+		for (int i = 0; i < totalRow; ++i) {
+			for (int j = 0; j < totalCol; ++j) {
+				result[i][j] = 0;
+			}
+		}
+		
+		// Modify the result with the corresponding value in Nx3 array
+		for (int i = 0; i < list.length; ++i) {
+			result[list[i][1]][list[i][2]] = list[i][0];
+		}
+
+		return result;
 	}
 
 	/**
 	 * Unfolds the array in a spiral order clockwise starting from index 0,0
 	 */
 	public static int[] spiralUnfolding(int[][] array){
-		// DELETE THE LINE BELOW ONCE YOU IMPLEMENT THE CALL!
-		throw new RuntimeException("not implemented!");
+		// Find the total numbers of rows and columns
+		int totalRow = array.length;
+		int totalCol = array[0].length;
+		// Initialize the result array that can hold all elements in the 2D array
+		int[] result = new int[totalRow * totalCol];
+
+		// Unfold the array, keep track of the current index on the result array
+		// Stop folding when all number has been tracked
+		int index = 0;
+		while (index < result.length) {
+			// Initialize a variable to keep track of the current spiral ring
+			int spiralRing = 0;
+			// For each spiral ring, we perform 4 operations
+			// Move right
+			for (int i = 0; i < totalCol; ++i) {
+				result[index] = array[0][i];
+				++index;
+			}
+
+			// Move downward
+			for (int j = 0; j < totalRow; ++j) {
+				result[index] = array[j][totalCol - 1];
+				++index;
+			}
+
+			//
+		}
 	}
 
 	/**
@@ -138,8 +234,51 @@ public class NumberProcessor {
 	 * Odd numbers are rotated to the left.
 	 */
 	public static void arrayRotation(int[] array){
-		// DELETE THE LINE BELOW ONCE YOU IMPLEMENT THE CALL!
-		throw new RuntimeException("not implemented!");
+		// Shift all even numbers to the right
+		int firstEvenIndex = -1;
+		int prevEvenIndex = -1;
+		int tempEvenVal = 0;
+		for (int i = 0; i < array.length; ++i) {
+			if (array[i] % 2 == 0) {
+				// Check if it's the first occurence
+				if (firstEvenIndex == -1) {
+					firstEvenIndex = i;
+					prevEvenIndex = i;
+					continue;
+				}
+				tempEvenVal = array[i];
+				array[i] = array[prevEvenIndex];
+				prevEvenIndex = i;
+			}
+		}
+		// Move the last even number to the first occurence position
+		// if there are at least 2 even numbers in the array
+		if (firstEvenIndex != prevEvenIndex) {
+			array[firstEvenIndex] = tempEvenVal;
+		}
+
+		// Shift all even numbers to the right
+		int firstOddIndex = -1;
+		int prevOddIndex = -1;
+		int tempOddVal = 0;
+		for (int i = array.length - 1; i >= 0; --i) {
+			if (array[i] % 2 == 1) {
+				// Check if it's the first occurence
+				if (firstOddIndex == -1) {
+					firstOddIndex = i;
+					prevOddIndex = i;
+					continue;
+				}
+				tempOddVal = array[i];
+				array[i] = array[prevOddIndex];
+				prevOddIndex = i;
+			}
+		}
+		// Move the last odd number to the first occurence position
+		// if there are at least 2 odd numbers in the array
+		if (firstOddIndex != prevOddIndex) {
+			array[firstEvenIndex] = tempOddVal;
+		}
 	}
 
 	/**
@@ -165,7 +304,73 @@ public class NumberProcessor {
 	 * Main method. Is not tested by the tester, create your own tests here!
 	 */
 	public static void main(String[] args){
-		//pass
+		System.out.println("Method 1:");
+		System.out.println(isCoprime(4, 9));
+		System.out.println(isCoprime(7, 49));
+		System.out.println();
+
+		System.out.println("Method 2:");
+		System.out.println(isPalindrome(12321));
+		System.out.println(isPalindrome(123221));
+		System.out.println();
+
+		System.out.println("Method 3:");
+		System.out.println(isFibonacciPrime(5));
+		System.out.println(isFibonacciPrime(8));
+		System.out.println();
+
+		System.out.println("Method 4:");
+		System.out.println(countCollatzSteps(97));
+		System.out.println(countCollatzSteps(1));
+		System.out.println();
+		
+		System.out.println("Method 5:");
+		boolean[] arr1 = {true, true, false, true, false, false};
+		sieveOfEratosthenes(arr1);
+		for (int i = 0; i < arr1.length; ++i) {
+			System.out.print(arr1[i] + " ");
+		}
+		System.out.println();
+		boolean[] arr2 = {true, false, false};
+		sieveOfEratosthenes(arr2);
+		for (int i = 0; i < arr2.length; ++i) {
+			System.out.print(arr2[i] + " ");
+		}
+		System.out.println("\n");
+
+		System.out.println("Method 6:");
+		int[][] arr3 = {{7,2,5},{1,0,2},{-4,3,3}};
+		int[][] result1 = list2array(arr3);
+		for (int i = 0; i < result1.length; ++i) {
+			for (int j = 0; j < result1[i].length; ++j) {
+				System.out.print(result1[i][j] + " ");
+			}
+			System.out.print(",");
+		}
+		System.out.println();
+		int[][] arr4 = {{1,0,0},{2,0,1},{3,1,0},{4,1,1}};
+		int[][] result2 = list2array(arr4);
+		for (int i = 0; i < result2.length; ++i) {
+			for (int j = 0; j < result2[i].length; ++j) {
+				System.out.print(result2[i][j] + " ");
+			}
+			System.out.print(",");
+		}
+		System.out.println("\n");
+		
+		System.out.println("Method 8:");
+		int[] arr5 = {1,2,3,4,5,6,7,8,9};
+		arrayRotation(arr5);
+		for (int i = 0; i < arr5.length; ++i) {
+			System.out.print(arr5[i] + " ");
+		}
+		System.out.println();
+		int[] arr6 = {5,7,1,6,8,3};
+		arrayRotation(arr6);
+		for (int i = 0; i < arr6.length; ++i) {
+			System.out.print(arr6[i] + " ");
+		}
+		System.out.println("\n");
 	}
 
 }
